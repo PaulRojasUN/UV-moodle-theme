808337f5ee64e654044567f0c0d22f8f588785ea
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-05-03 11:44:54 -0500

    Config file

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Config theme Moove UV
+ *
+ * @package    theme_mooveuv
+ * @since      Moodle 4.0
+ * @author     Iader E. García Gómez <iadergg@gmail.com>
+ * @copyright  2022 Área de Nuevas Tecnologías - DINTEV - Universidad del Valle <desarrollo.ant@correounivalle.edu.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$THEME->name = 'mooveuv';
+$THEME->sheets = [];
+$THEME->editor_sheets = [];
+$THEME->parents = ['moove', 'boost'];
+$THEME->enable_dock = false;
+$THEME->yuicssmodules = array();
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->requiredblocks = '';
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
