f11670aed304e116087abcb6c17b9873d7d57a7e
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-10-19 18:57:55 -0500

    Feat. Add slide url

--- classes/util/frontpage.php
+++ classes/util/frontpage.php
@@ -85,10 +85,12 @@ class frontpage {
         $defaultimage = new \moodle_url('/theme/mooveuv/pix/default_slide.jpg');
         for ($i = 1, $j = 0; $i <= $templatecontext['slidercount']; $i++, $j++) {
             $sliderimage = "sliderimage{$i}";
+            $sliderurl = "sliderurl{$i}";
 
             $templatecontext['slides'][$j]['key'] = $j;
             $templatecontext['slides'][$j]['active'] = $i === 1;
             $templatecontext['slides'][$j]['image'] = $this->$sliderimage ?: $defaultimage->out();
+            $templatecontext['slides'][$j]['sliderurl'] = $this->$sliderurl;
         }
 
         return $templatecontext;
