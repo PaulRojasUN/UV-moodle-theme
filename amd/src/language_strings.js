3608b874bc5cd24cd22d1e86d39be4f60fc7f2a1
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-11-07 15:38:18 -0500

    Lang strings for election process

---
+++ amd/src/language_strings.js
@@ -0,0 +1,34 @@
+/*
+ * @module     theme_mooveuv/modal_login
+ * @author     2022 Iader E. Garcia Gomez <iadergg@gmail.com>
+ * @copyright  2022 Área de Nuevas Tecnologías - DINTEV - Universidad del Valle <desarrollo.ant@correounivalle.edu.co>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import $ from 'jquery';
+
+export const init = () => {
+
+    var urlLocation = window.location.pathname;
+    var urlSplit = urlLocation.split("/");
+    var locationSearch = window.location.search;
+
+    var arrayQuestionnaires = ["9"];
+
+    if (urlSplit.includes('questionnaire') && urlSplit.includes('view.php')) {
+
+        var parameter = locationSearch.split("=")[1];
+
+        if (arrayQuestionnaires.includes(parameter)) {
+            $('.complete > a').html("Participe del proceso de votación");
+        }
+    } else if (urlSplit.includes('questionnaire') && (urlSplit.includes('complete.php') || urlSplit.includes('preview.php'))) {
+        var parameter = locationSearch.split("=")[1];
+        if (arrayQuestionnaires.includes(parameter)) {
+            $('input[name=submit]').val("Votar");
+            $('.floatprinticon').hide();
+            $('.mod_questionnaire_completepage generalbox>h3').html('Gracias por participar en el proceso de votación.');
+            $('.mod_questionnaire_previewpage>h2').html('Previsualizando');
+        }
+    }
+};
